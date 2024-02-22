0c0c4379710ac1cad40578ebbb152c6727a22f20
Mina Demian <mina@minademian.com>
2024-02-22 02:15:30 +0100

    chore: set up prod build

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const nextConfig = {
+  output: 'export',
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
