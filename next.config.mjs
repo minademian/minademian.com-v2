d232f89219f37dd1b8ff8bc3ed667149f75db7ee
Mina Demian <mina@minademian.com>
2024-03-04 18:56:50 +0100

    chore: default when not exporting

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 
+import { dir } from 'console';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
@@ -7,15 +8,17 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+};
+
+/*
   output: 'export',
   distDir: 'v2.0',
   images: {
     unoptimized: true,
   },
   trailingSlash: true,
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
-};
-
+*/
 export default nextConfig;
