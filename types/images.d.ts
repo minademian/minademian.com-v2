c0a89b0261a03b453144aa420f0997b6b599e8c8
Mina Demian <mina.demian@cabonline.com>
2025-10-07 02:50:25 +0200

    fix(ci): image types

---
+++ types/images.d.ts
@@ -0,0 +1,38 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+// Image import declarations for production builds
+declare module '*.png' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpeg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.gif' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.webp' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.svg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.ico' {
+  const src: string;
+  export default src;
+}
